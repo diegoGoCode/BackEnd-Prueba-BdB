ed83e00e148dd2a0ee4425ea0d10233cb8f1cfd8
diegoc360 <diego.diaz@complemento360.com>
2021-06-10 14:23:01 -0500

    se modificaron los mensajes de la restapi

--- src/main/java/com/bancobogota/prueba/util/MessageConstans.java
+++ src/main/java/com/bancobogota/prueba/util/MessageConstans.java
@@ -10,52 +10,52 @@ public class MessageConstans {
 	/**
 	 * Mensaje Error al guardar la persona
 	 */
-	public static final String ERROR_SAVE_PERSON = "Error al guardar la persona";
+	public static final String ERROR_SAVE_PERSON = "Failed to save person";
 	
 	/**
 	 * Mensaje Error al actualizar la persona
 	 */
-	public static final String ERROR_UPDATE_PERSON = "Error al actualizar la persona";
+	public static final String ERROR_UPDATE_PERSON = "Failed to update person";
 	
 	/**
 	 * Mensaje Persona actualizar no se encuentra
 	 */
-	public static final String PERSON_UPDATE_NOT_FOUND = "La persona a actualizar no se encuentra registrada";
+	public static final String PERSON_UPDATE_NOT_FOUND = "The person to update is not registered";
 	
 	/**
 	 * Mensaje persona aliminar no se encuentra
 	 */
-	public static final String PERSON_DELETE_NOT_FOUND = "La persona que desea eliminar no se encuentra registrada";
+	public static final String PERSON_DELETE_NOT_FOUND = "The person you want to delete is not registered";
 	
 	/**
 	 * Mensaje el id de la persona no puede ser nulo
 	 */
-	public static final String PERSON_ID_NOT_NULL = "El id de la persona no puede ser nulo";
+	public static final String PERSON_ID_NOT_NULL = "The id of the person cannot be null";
 	
 	/**
 	 * Mensaje persona no registrada
 	 */
-	public static final String PERSON_NOT_FOUND = "La persona no se encuentra registrada";
+	public static final String PERSON_NOT_FOUND = "The person is not registered";
 
 	/**
 	 * Mensaje nombre obligatorio
 	 */
-	public static final String NAME_REQUIRED = "El nombre es obligatorio";
+	public static final String NAME_REQUIRED = "The name is required";
 	
 	/**
 	 * Mensaje fecha de nacimiento obligatoria
 	 */
-	public static final String BIRTH_REQUIRED = "La fecha de nacimiento es obligatoria";
+	public static final String BIRTH_REQUIRED = "The date of birth is mandatory";
 	
 	/**
 	 * Mensaje tipo de persona obligatorio
 	 */
-	public static final String TYPE_REUIRED = "El tipo de persona es obligatorio";
+	public static final String TYPE_REUIRED = "The type of person is required";
 	
 	
 	/**
 	 * Mensaje tipo de persona no reconocido
 	 */
-	public static final String TYPE_NOT_EXIST = "El tipo de persona no se reconoce, valores permitidos (papa, mama, nin@)";
+	public static final String TYPE_NOT_EXIST = "The type of person is not recognized, allowed values ​​(father, mother, child)";
 }
 
